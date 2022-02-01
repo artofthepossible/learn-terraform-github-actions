f58b57e3d41e898ad4a6b9b16ce8ce1b326a488e
artofthepossible <73197728+artofthepossible@users.noreply.github.com>
2022-01-31 21:44:38 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -54,3 +54,12 @@ resource "aws_security_group" "web-sg" {
 output "web-address" {
   value = "${aws_instance.web.public_dns}:8080"
 }
+terraform {
+  cloud {
+    organization = "gh-actions-demos"
+
+    workspaces {
+      name = "gh-actions-demos"
+    }
+  }
+}
