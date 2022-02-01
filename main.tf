8bde35d4d0983fe66ed980fe858d34f04ce3eb4d
artofthepossible <73197728+artofthepossible@users.noreply.github.com>
2022-02-01 15:00:07 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,8 @@ terraform {
 
   cloud {
     organization = "gh-actions-demos"
-
+    hostname     = "app.terraform.io"
+    token        = "cxo3MxJ1k0n7dA.atlasv1.ILshRz44PouoMWneKm8fMLFDiuQN5UuzhXbAkaMhr0GTS4YHOvb9tHxa3ccAeEuZyZ8"
     workspaces {
       name = "gh-actions-demos"
     }
